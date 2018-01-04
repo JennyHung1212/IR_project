ea5811d455afc7de7d8655610b5c771492ca8aac
Victor Lee <victorlee850531@gmail.com>
2018-01-04 20:52:10 +0800

    new version of parse data

--- function/parse_data.py
+++ function/parse_data.py
@@ -11,6 +11,7 @@ import string
 import collections
 import math
 import csv
+import pickle
 
 # parse from data_job_posts_xs.csv and output a structured dictionary
 #   input: none
@@ -18,7 +19,7 @@ import csv
 def parse_data():
 
     # Read File
-    fread = open("../dataset/data_job_posts_xs.csv" ,"r")
+    fread = open("../dataset/data_job_posts_only_doc.csv" ,"r")
     csvCursor = csv.reader(fread)
 
     row_count = -1
@@ -38,11 +39,27 @@ def parse_data():
             # required quality
             # data_key[4] = row[4]
         else:
-            for j in range(0,5):
-                data_dict[row_count][data_key[j]] = row[j]
+            all_data_dict[row_count]["all"] = row[1]
+            if row[2] != "NA":
+                all_data_dict[row_count]["all"] += row[2]
+            if row[3] != "NA":
+                all_data_dict[row_count]["all"] += row[3]
+            if row[4] != "NA":
+                all_data_dict[row_count]["all"] += row[4]
+            # for j in range(0,5):
+            #     data_dict[row_count][data_key[j]] = row[j]
         row_count += 1
     fread.close()
-    return data_dict
+    return all_data_dict
+
+# Record the document frequency of each term and save it as a txt file (dictionary.txt)
+def construct_dictionary(data_dict):
+    term_array = []
+    for i in len(data_dict):
+        term_array_set = set(collect_text(data_dict, i, "all"))
+        term_array += term_array_set
+    term_array.sort()
+    record_df(term_array)
 
 # Tokenize the given file and also do stemming, stopwords removal and return the term list
 #   input:document id and what to collect
@@ -70,15 +87,11 @@ def collect_text(data_dict, post, column_title):
 #   input:term_array, which is a list with each document's terms
 def record_df(term_array):
     sorted_term_frequency = get_term_frequency_array(term_array)
-    index_counter = 1
-    for word in sorted_term_frequency:
-        result_content += "\n"+str(index_counter)+"\t"+word+"\t"+str(sorted_term_frequency[word])
-        index_counter += 1
-    fwrite = open("../dictionary.txt" ,"w")
-    fwrite.write(result_content)
+    print_out_df(sorted_term_frequency)
 
 # Get each term's frequency and sort them
 #   input:term_array, which is a list with each document's terms
+#   output:sorted_term_frequency, sorted_term_frequency[word]=df of such word
 def get_term_frequency_array(term_array):
     term_frequency = {}
     for word in term_array:
@@ -87,9 +100,23 @@ def get_term_frequency_array(term_array):
     sorted_term_frequency = collections.OrderedDict(sorted(term_frequency.items()))
     return(sorted_term_frequency)
 
+# Print out the txt file and the pkl file
+def print_out_df(sorted_term_frequency):
+
+    #txt file
+    index_counter = 1
+    result_content = "t_index\tterm\tdf"
+    for word in sorted_term_frequency:
+        result_content += "\n"+str(index_counter)+"\t"+word+"\t"+str(sorted_term_frequency[word])
+        index_counter += 1
+    fwrite = open("../dictionary.txt" ,"w")
+    fwrite.write(result_content)
+
+    #pkl file
+    fileObject = open("../dictionary",'wb')
+    pickle.dump(sorted_term_frequency,fileObject)
+    fileObject.close()
+
+
 data_dict = parse_data()
-term_array = []
-for i in len(data_dict):
-    temp_term = set(collect_text(data_dict, i, "JobDescription"))
-    term_array += temp_term
-record_df(term_array)
+construct_dictionary(data_dict)
