24a687d410b8a7e49e0860b798f5acd4a78b5b86
洪念綺 <jennyhung1212@gmail.com>
2018-01-14 13:59:23 +0800

    function modified

--- function/tf_idf.py
+++ function/tf_idf.py
@@ -7,6 +7,7 @@ import collections
 import math
 import csv
 import pickle
+import json
 import parse_data as pd
 
 #parse csv to list
@@ -41,33 +42,33 @@ def calculate_tf_idf(job_data, type_name):
 	fread = open("../dictionary.txt", "r")
 	str_text = fread.read()
 	fread.close()
-	temp_list = str_text.split("\n")[:-1]
+	temp_list = str_text.split("\n")[1:-1]
 
 	word_dict = {}
 	for i in range(0, len(temp_list)):
 		index = temp_list[i].split("\t")[0]
 		word = temp_list[i].split("\t")[1]
 		df = temp_list[i].split("\t")[2]
-		word_dict.update( { word:[index, df] } )
+			word_dict.update( { word:[index, df] } )
 
 	tf_idf_list = []
 	for i in range(0, len(job_data)):
+		print ("iteration " + str(i))
 		word_tf_idf = []
 		for word, index_df in word_dict.items():
 			tf = job_data[i]["all"].count(word)
 			idf = math.log10(len(job_data)/int(index_df[1]))
 			tf_idf = tf*idf
 			if tf != 0:
-				word_tf_idf.append( [index_df[0], word, tf_idf] )
+				word_tf_idf.append( [index_df[0], tf_idf] )
 
 		normalize = 0
 		for i in range(0, len(word_tf_idf)):
-			normalize += word_tf_idf[i][2]*word_tf_idf[i][2]
+			normalize += word_tf_idf[i][1]*word_tf_idf[i][1]
 		normalize = math.sqrt(normalize)
 
 		for i in range(0, len(word_tf_idf)):
-			word_tf_idf[i][2] = word_tf_idf[i][2]/normalize
-			print (word_tf_idf[i][2])
+			word_tf_idf[i][1] = word_tf_idf[i][1]/normalize
 
 		tf_idf_list.append(word_tf_idf)
 		
@@ -75,9 +76,9 @@ def calculate_tf_idf(job_data, type_name):
 	print (tf_idf_list)
 
 	if type_name == "generate":
-		filename = "../tf_idf.p"
-		fread = open(filename, "wb")
-		pickle.dump(tf_idf_list, fread)
+		filename = "../tf_idf.json"
+		fread = open(filename, "w")
+		json.dump(tf_idf_list, fread)
 		fread.close()
 	else:
 		return (tf_idf_list)
